0c63f329cf1dcc4a79f6b460a94d978ee2b7e7d6
Arty <arty_h41@mail.ru>
2024-06-22 22:20:04 +0300

    setting drf

--- drfsite/book/serializers.py
+++ drfsite/book/serializers.py
@@ -3,7 +3,9 @@ from rest_framework import serializers
 from .models import *
 
 
+
+
 class PostSeriaLizer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('title', 'genre_id',)
+        fields = ('title', 'genre',)
--- drfsite/book/views.py
+++ drfsite/book/views.py
@@ -1,14 +1,27 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import *
 from .serializers import PostSeriaLizer
 
 
-class BookAPIView(generics.ListAPIView):
-    """
+# class BookAPIView(generics.ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSeriaLizer # serializers.py
 
-    """
+class BookAPIView(APIView):
+    def get(self, request):
+        lst = Post.objects.all().values()
 
-    queryset = Post.objects.all()
-    serializer_class = PostSeriaLizer # serializers.py
+        return Response({'title': list(lst)})
+
+    def post(self, request):
+        post_new = Post.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            genre=request.genre_set['genre'],
+            # genre=Genre.genre_set.set(request.data['genre']),
+        )
+        return Response({'post': model_to_dict(post_new)})
